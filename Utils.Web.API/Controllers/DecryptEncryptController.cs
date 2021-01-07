e05be230371311ecebd18e42781f143da763fa72
Luis Gutierrez <aauronn@gmail.com>
2021-01-07 11:47:01 -0500

    Add project files.

---
+++ Utils.Web.API/Controllers/DecryptEncryptController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Web.API.Models;
+using Utils.Common.Extensions;
+
+namespace Utils.Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DecryptEncryptController : ControllerBase
+    {
+        private readonly ILogger<DecryptEncryptController> _logger;
+
+        public DecryptEncryptController(ILogger<DecryptEncryptController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("encrypt")]
+        public IActionResult Encrypt([FromBody] EncryptDecryptModelDTO model)
+        {
+            var DecryptedText = model.Text.EncryptString(model.Key);
+
+            return Ok(DecryptedText);
+        }
+
+        [HttpPost("decrypt")]
+        public IActionResult Decrypt([FromBody] EncryptDecryptModelDTO model)
+        {
+            var DecryptedText = model.Text.DecryptString(model.Key);
+
+            return Ok(DecryptedText);
+        }
+
+
+    }
+}
---
+++ Utils.Web.API/Models/EncryptDecryptModelDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Web.API.Models
+{
+    public class EncryptDecryptModelDTO
+    {
+        public string Text { get; set; }
+        public string Key { get; set; }
+    }
+}
